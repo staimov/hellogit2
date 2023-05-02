78cde02aa5103553a860c210737ecf3e26879289
staimov <staimov@mail.ru>
2023-05-02 15:59:35 +0500

    greet property added!

--- src/main/java/com/staimov/hellogit/controller/HelloController.java
+++ src/main/java/com/staimov/hellogit/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.staimov.hellogit.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${greet}")
+    private String greet;
+
     @GetMapping("/hello")
     public  String helloWorld() {
-        return "Hello, World!";
+        return greet;
     }
 }
