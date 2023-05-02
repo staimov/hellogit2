b793fecdaaec88b6aa32af88d52e639b669bfe6d
staimov <staimov@mail.ru>
2023-05-02 17:20:08 +0500

    Update Student.java

--- src/main/java/com/staimov/hellogit/json/Student.java
+++ src/main/java/com/staimov/hellogit/json/Student.java
@@ -2,12 +2,14 @@ package com.staimov.hellogit.json;
 
 public class Student {
     private String firstName;
+    private String lastName;
 
     public Student() {
     }
 
-    public Student(String firstName) {
+    public Student(String firstName, String lastName) {
         this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getFirstName() {
@@ -17,11 +19,20 @@ public class Student {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+    
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Student{");
         sb.append("firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
         sb.append('}');
         return sb.toString();
     }
