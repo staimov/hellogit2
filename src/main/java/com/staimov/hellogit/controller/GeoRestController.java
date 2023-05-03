810d370fcabe6e39219c80e807b8d7337b971bf7
staimov <staimov@mail.ru>
2023-05-03 11:09:38 +0500

    Logging added

--- src/main/java/com/staimov/hellogit/controller/GeoRestController.java
+++ src/main/java/com/staimov/hellogit/controller/GeoRestController.java
@@ -1,6 +1,8 @@
 package com.staimov.hellogit.controller;
 
 import com.staimov.hellogit.json.Geo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class GeoRestController {
+    private static final Logger logger = LoggerFactory.getLogger(GeoRestController.class);
+
     @GetMapping("/geo")
     public Geo getGeo() {
-       return new Geo("Novolazarevskaya",
+        logger.info("Geo get request");
+        return new Geo("Novolazarevskaya",
                -70.773889, 11.865);
     }
 }
